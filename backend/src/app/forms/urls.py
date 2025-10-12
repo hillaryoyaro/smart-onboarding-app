3556b1d13ea303fe5a9d8faf88bf45828a578540
oyaro <hillaryoyaroh@gmail.com>
2025-10-12 13:11:49 +0300

    update url

--- backend/src/app/forms/urls.py
+++ backend/src/app/forms/urls.py
@@ -1,8 +1,10 @@
-# src/app/form/urls.py
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FormViewSet
 
 router = DefaultRouter()
-router.register(r"", FormViewSet, basename="forms")
+router.register("", FormViewSet, basename="forms")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+]
